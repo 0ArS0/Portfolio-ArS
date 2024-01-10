c3b0dcc49e48962b192b3b08c33380fb6fee4e0f
0ArS0 <arthurmonteiro20172018@gmail.com>
2024-01-10 16:57:31 -0300

    Tirando magic numbers; Adicionando git e linkeDin no start; Mudando o texto em Start; Removendo o switch do dark mode; Mudanças no estilo geral em diversas partes; Animação no texto do start

--- Portfolioars/src/components/AboutMe/AboutMe.jsx
+++ Portfolioars/src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { motion, useScroll } from 'framer-motion'
 import './AboutMe.css';
-import { GiEnlightenment } from "react-icons/gi";
-import { IoFileTrayFullSharp } from "react-icons/io5";
 import { useTranslation } from 'react-i18next';
 
 export const AboutMe = () => {
@@ -48,28 +46,19 @@ export const AboutMe = () => {
                 }}
                 className="aboutme">
                 <h2 className='title title-aboutme'>{t("aboutme.title")}</h2>
-                <div className='cards-aboutme-container'>
-                    <div className="card-aboutme">
-                        <IoFileTrayFullSharp size={50} className='card-aboutme-icon' />
-                        <h4 className='title'>{t("aboutme.title-projects")}</h4>
-                        <p className='text'>10+ {t("aboutme.text-projects")}</p>
+                <div className='aboutme-container'>
+
+                    <div className='text-aboutme-container'>
+                        <p className='text text-aboutme'>
+                            {t('aboutme.text-aboutme1')}
+                        </p>
+                        <p className='text text-aboutme'>
+                            {t('aboutme.text-aboutme2')}
+                        </p>
+                        <button className='button-aboutme' style={{ '--eixoX': `${eixoX}px`, '--eixoY': `${eixoY}px` }} ref={btnRef}>
+                            <span className='text'>{t('aboutme.button-download-text')}</span>
+                        </button>
                     </div>
-                    <div className="card-aboutme">
-                        <GiEnlightenment size={50} className='card-aboutme-icon' />
-                        <h4 className='title'>{t("aboutme.title-experience")}</h4>
-                        <p className='text'>{t("aboutme.text-experience")}</p>
-                    </div>
-                </div>
-                <div className='text-aboutme-container'>
-                    <p className='text text-aboutme'>
-                        {t('aboutme.text-aboutme1')}
-                    </p>
-                    <p className='text text-aboutme'>
-                        {t('aboutme.text-aboutme2')}
-                    </p>
-                    <button className='button-aboutme' style={{ '--eixoX': `${eixoX}px`, '--eixoY': `${eixoY}px` }} ref={btnRef}>
-                        <span className='text'>{t('aboutme.button-download-text')} CV</span>
-                    </button>
                 </div>
             </motion.section>
         </div>
--- Portfolioars/src/components/Start/Start.jsx
+++ Portfolioars/src/components/Start/Start.jsx
@@ -3,34 +3,115 @@ import { Image } from 'react-bootstrap'
 import { motion } from 'framer-motion'
 import './Start.css';
 import { useTranslation } from 'react-i18next';
+import { useReadLocalStorage } from 'usehooks-ts';
 
 export const Start = () => {
     const [t, i18n] = useTranslation("global")
+    const theme = useReadLocalStorage('dark')
+
+    const startText = {
+        initial: {
+            opacity: 1,
+        },
+        animate: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.08,
+            }
+        }
+    }
+
+    const startWords = {
+        initial: {
+            opacity: 0,
+            y: 30,
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 1
+            }
+        }
+    }
 
     return (
-        <div className="start-container" id="start">
-            <motion.section
-                variants={{
-                    hidden: { opacity: 0, y: 80 },
-                    visible: { opacity: 1, y: 0 }
-                }}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className="start"
-            >
-                <div className='profile-pic-container'>
+        <div id="start">
+            <section className="start" >
+                <motion.div
+                    variants={{
+                        hidden: { opacity: 0, y: 80 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 1, delay: 0.25 }}
+                    className='profile-pic-container'>
                     <Image src='src/assets/img/ProfilePic.png' className='profile-pic' roundedCircle fluid />
-                </div>
+                </motion.div>
                 <div className="initial-text-container">
                     <div className="title-container">
-                        <h1 className='title initial-title'>{t("start.text-name")}<span className='dot-style'>.</span></h1>
-                        <p className='text fullstack-text'>&lt;<span className='fullstack-text-color'>{t("start.text-dev2")}</span>/&gt;</p>
-                    </div>
-                    <p className='text initial-text'>{t("start.text-description")}<span className='blink'>|</span></p>
-                </div>
+                        <motion.h1
+                            variants={startText}
+                            initial="initial"
+                            animate="animate"
+                            className='title initial-title'>
+                            {t("start.text-name").split(" ").map((word, index) =>
+                                <motion.span
+                                    className='span-text fullstack-text-color'
+                                    variants={startWords}
+                                    key={word + '-' + index}>
+                                    {word}&nbsp;
+                                </motion.span>
+                            )}
+                        </motion.h1>
 
-            </motion.section>
+                    </div>
+                    <motion.p
+                        variants={startText}
+                        initial="initial"
+                        animate="animate"
+                        className='text initial-text'>
+
+                        {t("start.text-description").split(" ").map((word, index) => <>
+                            <motion.span
+                                className='span-text'
+                                variants={startWords}
+                                key={word + '-' + index}>
+                                {word}&nbsp;
+                            </motion.span>
+                        </>
+                        )}<motion.span variants={startWords} className='blink'>|</motion.span>
+
+                    </motion.p>
+                    <div className='social'>
+                        <motion.a
+                            variants={startText}
+                            initial="initial"
+                            animate="animate"
+                            whileHover={{ y: -4 }}
+                            whileTap={{ scale: 0.9 }}
+                            href='https://github.com/0ArS0' target='_blank'>
+                            <motion.img
+                                className='span-text'
+                                variants={startWords}
+                                src={theme === 'light' ? 'src/assets/img/githubDark.svg' : 'src/assets/img/githubLight.svg'} width={40} />
+                        </motion.a>
+                        <motion.a
+                            variants={startText}
+                            initial="initial"
+                            animate="animate"
+                            whileHover={{ y: -4 }}
+                            whileTap={{ scale: 0.9 }}
+                            href='https://www.linkedin.com/in/arthurmonteiro-ars/' target='_blank'>
+                            <motion.img
+                                className='span-text'
+                                variants={startWords}
+                                src={theme === 'light' ? 'src/assets/img/linkedInDark.svg' : 'src/assets/img/linkedInLight.svg'} width={45} />
+                        </motion.a>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
